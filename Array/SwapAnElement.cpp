ea73a4fd398ec5f215dfa01a79018e5a324690c2
Khushi Sharma <khushiiee.26@gmail.com>
2026-09-08 23:21:56 +0530

    Swap Alternate Elements

---
+++ Array/SwapAnElement.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int arr[6] = {10, 20, 30, 40, 50, 60};
+
+  for (int i = 0; i < sizeof(arr) / sizeof(int) - 1; i += 2)
+  {
+    swap(arr[i], arr[i + 1]);
+  }
+
+  for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
+  {
+    cout << arr[i] << " ";
+  }
+}
--- Array/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-arr.pop_back();
-
-  // cout << "Updated Array isL";
-  // for (int i = 0; i < arr.size(); i++)
-  // {
-  //   cout << arr[i] << " ";
-  // }
